3fb0412d9ad7ad735bc3151489cf2666471e553c
tlagusejr <starc13@naver.com>
2023-11-14 17:43:35 +0900

    git init

---
+++ src/common.js
@@ -0,0 +1,23 @@
+import {
+    reactive,
+    computed,
+    toRefs
+} from 'vue';
+
+import axios from 'axios';
+
+
+
+const api = async (url, method, data) => {
+    return (await axios({
+        method: method,
+        url,
+        data
+    }).catch(e => {
+        console.log(e);
+    })).data;
+}
+
+export {
+    api
+};
