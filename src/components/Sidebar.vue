48f3931e54a46a46c14d062b73d9b2444b575463
syeej <syee2joe@gmail.com>
2023-11-16 15:19:59 +0900

    sidebar 이미지 수정 기능 구현

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,8 +2,9 @@
   <aside>
     <div class="frame-profileinfo">
       <div class="frame-profile">
-        <img class="profile-img"
-              :src="myInfo.profilePhoto"
+        <img
+            class="profile-img"
+              :src="previewPhoto || myInfo.profilePhoto"
              alt="Image"/><br/>
         <span class="text-nickname" v-text="myInfo.nickname"></span>
         <span class="text-email" v-text="myInfo.email"></span>
@@ -12,15 +13,16 @@
           <input
               type="file"
               id="inputImgFile"
+              ref="imgfileInput"
               name="profilePhoto"
               accept=".jpg, .png"
               @change="previewImg"
-              required/>
+              required hidden/>
           <input
               id="updateimg"
               class="input-submit"
               type="button"
-              @click="uploadProfileImg()"
+              @click="updateProfileImg()"
               value="변경 저장"/>
         </div>
       </div>
@@ -60,9 +62,9 @@
 import {onMounted, reactive, ref} from 'vue';
 import axios from "axios";
 import {useRoute} from "vue-router";
+import {api} from "../common.js";
 
 const route = useRoute();//CompositionAPI 매칭된 라우트 (OptionAPI : this.$route)
-
 const getDataErr = reactive({});
 const myInfo = ref({
   "userId":"",
@@ -71,6 +73,8 @@ const myInfo = ref({
   "email" : "",
   "profilePhoto" : ""
 });
+const imgfileInput = ref(null);
+const previewPhoto = ref(null);
 
 async function getData(){
   try {
@@ -86,23 +90,40 @@ onMounted(()=>{
   console.log("sidebar onmount");
   getData();
 });
-let encodeImg = ref(null);
 
+//이미지 미리보기 기능
 const previewImg = (event) => {
-  let input = event.target;
+  let inputFile = event.target;
   let reader = new FileReader();
 
   reader.onload = () => {
-    encodeImg.value = reader.result;
+    previewPhoto.value = reader.result;
+    console.log(myInfo.previewPhoto);
     document.getElementById('updateimg').style.display='block';
   };
-  reader.readAsDataURL(input.files[0]);
+  reader.readAsDataURL(inputFile.files[0]);
+  document.getElementById("")
 };
-  // 프로필 이미지 업로드
-  function uploadProfileImg(){
-    
-  }
 
+//이미지 수정 기능
+function updateProfileImg(){
+  if(!confirm("정말 수정하시겠습니까?")) {
+      alert("취소되었습니다.");
+      window.location.reload();
+   }else {
+     let fd = new FormData();
+     fd.append("profilePhoto", imgfileInput.value.files[0]);
+     axios.put("http://localhost:8081/users/setting/img/" + route.params.user_id, fd,
+         {headers: {'Content-Type': 'multipart/form-data'}})
+         .then(res => {
+           console.log(res);
+           window.location.reload();
+         }).then(error => {
+       console.log(error);
+     });
+     console.log("수정완료");
+   }
+  }
 </script>
 
 <style src="../assets/css/sidebar.css" scoped>
