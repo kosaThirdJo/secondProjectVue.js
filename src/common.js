c3d6464788a639d03c4e8da6daa2157bc999f9bc
tlagusejr <starc13@naver.com>
2023-12-03 14:56:53 +0900

    배포시 ip 변경

--- src/common.js
+++ src/common.js
@@ -5,13 +5,17 @@ import {
 } from 'vue';
 
 import axios from 'axios';
-
+import {useRoute} from "vue-router";
 
 
 // urn 이거 참조 https://www.elancer.co.kr/blog/view?seq=74
 const apiToken = async (urn, method, data, token) => {
-    route.url
-    const url = "http://43.201.149.206:8081/" + urn
+    var forwardIp = document.URL.match(/\/\/(.*?)\//);
+    if (forwardIp) {
+        var ip = forwardIp[1].split(':')[0];
+    }
+    const url = "http://"+ ip + ":8081/" + urn
+    console.log(url)
     // URL : URL 인대 URL URL이 같으면 생략 가능
     return (await axios({
         url,
@@ -21,34 +25,38 @@ const apiToken = async (urn, method, data, token) => {
             Authorization: token
         }
     }).catch(e => {
-        console.log("http://43.201.149.206:8081/" + urn)
-        console.log(e);
         return { data: e}; //error 발생 시 e 반환
     })).data
 }
 const api = async (urn, method, data) => {
-    const url = "http://43.201.149.206:8081/" + urn
+    var forwardIp = document.URL.match(/\/\/(.*?)\//);
+    if (forwardIp) {
+        var ip = forwardIp[1].split(':')[0];
+    }
+    const url = "http://"+ ip + ":8081/" + urn
+    console.log(url)
     // URL : URL 인대 URL URL이 같으면 생략 가능
     return (await axios({
         url,
         method,
         data
     }).catch(e => {
-        console.log("http://43.201.149.206:8081/" + urn)
-        console.log(e);
         return { data: e}; //error 발생 시 e 반환
     })).data
 }
 
 const loginApi = async (urn, method, data) => {
-    const url = "http://43.201.149.206:8081/" + urn
+    var forwardIp = document.URL.match(/\/\/(.*?)\//);
+    if (forwardIp) {
+        var ip = forwardIp[1].split(':')[0];
+    }
+    const url = "http://"+ ip + ":8081/" + urn
+    console.log(url)
     return (await axios({
         url, method, data, headers: {
             "X-Requested-With": "XMLHttpRequest"
         }
     }).catch(e => {
-        console.log(url);
-        console.log(e);
         return{data: e};
     }))
 }
